4bddfa56144fd2302ef80c3d41d86d329140ea8c
Dmitry Lyapin <dma@cranbee.com>
2017-07-01 12:51:05 +0300

    Add unpack() to the public interface

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let Scanner = require('./lib/scanner.js');
 let Parser = require('./lib/parser.js');
 let Packer = require('./lib/packer.js');
+let Executor = require('./lib/executor.js');
 
 module.exports = {
     scan: Scanner.scan,
     parse: Parser.parse,
-    pack: Packer.pack
+    pack: Packer.pack,
+    unpack: Packer.unpack,
+    execute: Executor.execute
 };
